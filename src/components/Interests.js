c5bc8766ee8c5dd58877b78085e64445c89ef991
“gursham” <Gursham2001@hotmail.com>
2022-05-09 23:27:03 +0100

    improved portfolio

--- src/components/Interests.js
+++ src/components/Interests.js
@@ -8,11 +8,11 @@ function Interest() {
           <br />
           <h3 className='interest-h3'>Star Wars.</h3>
           <br />
-          <p className='interest-description'>Star Wars has been part of my life since I was a kid, favorite movie would be The Empire Strikes Back </p>
+          <p className='interest-description'>Star Wars has been part of my life since I was a kid, favorite movie would be The Empire Strikes Back.</p>
           <br />
           <h3 className='interest-h3'>Formula Racing</h3>
           <br />
-          <p className='interest-description'>Recently over the last year or so i have been following Formula 1, 2 and 3 and have grown a real strong interest and passion for racing, I have goals to visit some of the most popular Grand Prix around the world in the future</p>
+          <p className='interest-description'>Recently over the last year or so i have been following Formula 1, 2 and 3 and have grown a real strong interest and passion for racing, I have goals to visit some of the most popular Grand Prix around the world in the future.</p>
           <br />
           <h3 className='interest-h3'>Playing Pool.</h3>
           <br />
@@ -20,11 +20,17 @@ function Interest() {
           <br />
           <h3 className='interest-h3'>Watching Football.</h3>
           <br />
-          <p className='interest-description'>Ive recently grew an interest in watching Football, supporting Manchester United, I found it a way to unwind and have fun</p>
+          <p className='interest-description'>Ive recently grew an interest in watching Football, supporting Manchester United, I found it a way to unwind and have fun.</p>
+          <br />
+          <h3 className='interest-h3'>Reading</h3>
+          <br />
+          <p className='interest-description'>Reading books like The Alchemist has sparked a personal interest in reading, at the moment I am reading The School of Greatness by Lewis Howes.</p>
           <br />
           <h3 className='interest-h3'>Travelling.</h3>
           <br />
-          <p className='interest-description'>I have always loved travelling to new places and experiencing new cultures, I have visted india a few times have had a roadtrip to Scotland. I plan to travel more and experience all ways of life. </p>
+          <p className='interest-description'>I have always loved travelling to new places and experiencing new cultures, I have visted india a few times have had a roadtrip to Scotland. I plan to travel more and experience all ways of life.</p>
+          <br />
+          <br />
       </div>
     </div>
   )
