c20d3816e5a3b87888be556ff37691b8b3c36bb9
“gursham” <Gursham2001@hotmail.com>
2022-05-16 22:51:42 +0100

    icons added

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -5,26 +5,30 @@ function Skills() {
           <h1 className='headers'>SKILLS.</h1>
             <hr className='hr1'></hr>
             <div className='skills-icons'>
-            <i className="devicon-html5-plain-wordmark"></i>
-            <i className="devicon-css3-plain-wordmark"></i>
-            <i className="devicon-javascript-plain"></i>
-            <i className="devicon-react-original-wordmark"></i>
-            <i className="devicon-bulma-plain"></i>
-            <i className="devicon-sass-original"></i>
-            <i className="devicon-vscode-plain-wordmark"></i>
-            <i className="devicon-python-plain-wordmark"></i>
-            <i className="devicon-django-plain"></i>
-            <i className="devicon-postgresql-plain-wordmark"></i>
-            <i className="devicon-express-original"></i>
-            <i className="devicon-nodejs-plain"></i>
-            <i className="devicon-mongodb-plain-wordmark"></i>
-            <i className="devicon-npm-original-wordmark"></i>
-            <i className="devicon-yarn-plain"></i>
-            <i className="devicon-heroku-original-wordmark"></i>
-            <i className="devicon-git-plain"></i>
-            <i className="devicon-github-original-wordmark"></i>
-            <i className="devicon-trello-plain-wordmark"></i>
-            <i className="devicon-slack-plain-wordmark"></i>
+            <i className="devicon-html5-plain-wordmark" id='blue'></i>
+            <i className="devicon-css3-plain-wordmark" id='white'></i>
+            <i className="devicon-javascript-plain" id='blue'></i>
+            <i className="devicon-react-original-wordmark" id='white'></i>
+            <i className="devicon-bulma-plain" id='blue'></i>
+            <i className="devicon-sass-original" id='white'></i>
+            <i className="devicon-vscode-plain-wordmark" id='blue'></i>
+            <i className="devicon-python-plain-wordmark" id='white'></i>
+            <i className="devicon-django-plain" id='blue'></i>
+            <i className="devicon-postgresql-plain-wordmark" id='white'></i>
+            <i className="devicon-express-original" id='blue'></i>
+            <i className="devicon-nodejs-plain" id='white'></i>
+            <i className="devicon-mongodb-plain-wordmark" id='blue'></i>
+            <i className="devicon-npm-original-wordmark" id='white'></i>
+            <i className="devicon-yarn-plain" id='blue'></i>
+            <i className="devicon-heroku-original-wordmark" id='white'></i>
+            <i class="devicon-jquery-plain-wordmark" id='blue'></i>
+            <i className="devicon-git-plain" id='white'></i>
+            <i className="devicon-github-original-wordmark" id='blue'></i>
+            <i className="devicon-trello-plain-wordmark" id='white'></i>
+            <i className="devicon-slack-plain-wordmark" id='blue'></i>
+            <i class="devicon-wordpress-plain-wordmark" id='white'></i>
+            <i class="devicon-woocommerce-plain-wordmark" id='blue'></i>
+            <i class="devicon-eslint-original-wordmark" id='white'></i>
             </div>
         </div>
       // </div>
